99e1324c837b0b91bda26f96b6dfbf3e06d3b912
hazharmaroufi <hazhmar@gmail.com>
2024-06-29 20:05:37 +0330

    problem fixed and challeng solved

--- script.js
+++ script.js
@@ -8,29 +8,29 @@ btn.style.color = "white";
 btn.style.borderRadius = "8px";
 btn.style.margin = "8px 16px"
 btn.textContent = "How many pixels? "
-btn.addEventListener("click", () => {
+btn.addEventListener("click", sketch);
+
+function sketch() {
+  container.innerHTML = '';
   do {
     pixels = prompt("Please enter Pixels: ");
   } while (pixels > 100);
-})
-container.style.display = "flex";
-container.style.flexWrap = "wrap";
-container.style.width = `${pixels * 20}px`;
-container.style.height = `${pixels * 20}px`;
-const div = document.createElement("div");
-
-for (let j = 1; j <= pixels; j++) {
-  for (let i = 1; i <= pixels; i++) {
-    const div = document.createElement("div");
-    container.appendChild(div);
-    div.setAttribute("id", "cell");
-    // div.textContent = `${i}`;
-    div.style.width = "20px";
-    div.style.height = "20px";
-    div.style.backgroundColor = "#f9fafb"
+  container.style.display = "flex";
+  container.style.flexWrap = "wrap";
+  container.style.width = `${pixels * 20}px`;
+  container.style.height = `${pixels * 20}px`;
+  const div = document.createElement("div");
+  for (let j = 1; j <= pixels; j++) {
+    for (let i = 1; i <= pixels; i++) {
+      const div = document.createElement("div");
+      container.appendChild(div);
+      div.style.width = "20px";
+      div.style.height = "20px";
+      div.style.backgroundColor = "#f9fafb"
+    }
   }
+  container.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "#881337";
+  })
+
 }
-container.addEventListener("mouseover", (e) => {
-  e.target.style.backgroundColor = "#881337";
-  console.log("hovered");
-})
